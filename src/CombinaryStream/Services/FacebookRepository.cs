446f0979b61df15a802d07225862cc3b04b7aacf
Florian Fida <f+git@fida.biz>
2019-08-30 17:19:15 +0200

    update facebook query

--- src/CombinaryStream/Services/FacebookRepository.cs
+++ src/CombinaryStream/Services/FacebookRepository.cs
@@ -17,9 +17,30 @@ namespace CombinaryStream.Services
         }
         public async Task<IEnumerable<StreamItem>> GetItemsAsync() {
             if (string.IsNullOrWhiteSpace(_connectionString) || _limit <= 0) return new StreamItem[0];
+
+            const string sql = @"
+                select
+                       'https://fb.com/' || p.page_id || '_' || p.id as ""Url"",
+                       'facebook' as ""ItemType"",
+                       p.story as ""Title"",
+                       p.message as ""Body"",
+                       p.created_time as ""PublishedAt"",
+                       a.media_url as ""ThumbnailUrl""
+
+                from post p
+                join attachment a on a.id = (
+                  select id from attachment ja
+                  where ja.id = p.id
+                  order by created_time desc
+                  limit 1
+                )
+                order by p.created_time desc
+                limit @limit
+            ";
+
             var items = new List<StreamItem>();
             using (var conn = new NpgsqlConnection(_connectionString)) {
-                items.AddRange(await conn.QueryAsync<StreamItem>("SELECT * FROM items LIMIT @limit", new {limit=_limit}));
+                items.AddRange(await conn.QueryAsync<StreamItem>(sql, new {limit=_limit}));
             }
             return items;
         }
